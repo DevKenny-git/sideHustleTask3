bcae9b1aac2486700671c95e8d2265c79cc91ef2
Shodipe Oladele <quickly2kenny@gmail.com>
2021-07-14 14:37:14 +0100

    Added table to viewdb.php script

--- viewdb.php
+++ viewdb.php
@@ -9,9 +9,12 @@
 		$con = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
 		$con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 		$stmt = $con->query('SELECT * FROM task');
+		echo "<table border='1'><tr><th>Activity</th><th>Status</th><tr>";
+
 		while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-			echo $row['activity'] .  ' '. $row['status'] . '<br>';
+			echo "<tr><td>" .$row['activity'] . "</td><td>" . $row['status'] . "</td></tr>";
 		}
+		echo "</table>";
 			
 	} catch(PDOException $e) {
 		echo "Connection failed: " . $e->getMessage();
